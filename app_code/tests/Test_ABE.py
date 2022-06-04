8757f816e5888223e68b7a16693521ffa542e9fe
sofiiafolv <92806105+sofiiafolv@users.noreply.github.com>
2022-06-05 00:02:46 +0300

    Update Test_ABE.py

--- app_code/tests/Test_ABE.py
+++ app_code/tests/Test_ABE.py
@@ -10,6 +10,7 @@ sys.path.append(root_folder)
 from board_elements.Fundamentals import Board
 from board_elements.AdvancedBoardElements import *
 from board_elements.SimpleBoardElements import ONE_Generator, ZERO_Generator
+
 from Test_SBE import Test_SBE
 
 
@@ -78,6 +79,21 @@ class Test_ABE(Test_SBE):
         self.set_board_01()
         self.board.connect_pins(
             self.src_A.get_outputs()[0], self.element.get_inputs()[2]
+        )
+
+    def set_board_010100(self):
+        self.set_board_01()
+        self.board.connect_pins(
+            self.src_A.get_outputs()[0], self.element.get_inputs()[2]
+        )
+        self.board.connect_pins(
+            self.src_B.get_outputs()[0], self.element.get_inputs()[3]
+        )
+        self.board.connect_pins(
+            self.src_A.get_outputs()[0], self.element.get_inputs()[4]
+        )
+        self.board.connect_pins(
+            self.src_A.get_outputs()[0], self.element.get_inputs()[5]
         )
 
     # def set_shifters(self):
@@ -203,7 +219,7 @@ class Test_ABE(Test_SBE):
         )
 
     def test_Encoder_8_to_3(self):
-        """Testing 8-to-3 encoder"""
+        """Testing 4-to-2 encoder"""
         self.el = Encoder_8_to_3
         self.board.clear()
         self.element = self.board.create_element(self.el)
@@ -216,7 +232,20 @@ class Test_ABE(Test_SBE):
             [True, True, False, True],
         )
 
+    def test_Multiplexor(self):
+        """Testing  multiplexor"""
+        self.el = Multiplexor
+        self.board.clear()
+        self.element = self.board.create_element(self.el)
+        self.set_board_010100()
+        self.assertEqual(
+            [
+                self.element.get_outputs()[i].get_state()
+                for i in range(len(self.element.get_outputs()))
+            ],
+            [True],
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
-
